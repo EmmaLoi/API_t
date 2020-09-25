87792cfe658e7083f4dc5bc77aaa1735b5122777
Emma L <emmanuel.loi@neptuness.com>
2020-09-25 22:46:52 +0300

    Added HW_45_Add_Delete_Comment

--- src/test/java/HW_Hillel/JiraTests.java
+++ src/test/java/HW_Hillel/JiraTests.java
@@ -3,14 +3,25 @@ package HW_Hillel;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.lessThan;
 import static org.testng.Assert.assertEquals;
 
 
 public class JiraTests {
   String ticketId;
+  String commentURI;
+  String commentId;
   Response responseCreateIssue;
   Response responseGetCreatedIssue;
+  Response deleteIssueResponse;
+  Response checkDeletedIssueResponse;
+  Response addJiraCommentResponse;
+  Response deleteCommentResponse;
+  Response getDeletedCommentResponse;
 
   @Test
   public void jiraCreateIssue() {
@@ -53,5 +64,59 @@ public class JiraTests {
             extract().response();
     assertEquals(responseGetCreatedIssue.path("fields.summary"), "Main order flow broken");
     assertEquals(responseGetCreatedIssue.path("fields.creator.name"), "webinar5");
+
+    deleteIssueResponse =
+        given().
+            auth().preemptive().basic("webinar5", "webinar5").
+            contentType(ContentType.JSON).
+            when().
+            delete("https://jira.hillel.it/rest/api/2/issue/" + ticketId).
+            then().
+            statusCode(204).
+            extract().response();
+    deleteIssueResponse.print();
+
+    checkDeletedIssueResponse =
+        given().
+            auth().preemptive().basic("webinar5", "webinar5").
+            contentType(ContentType.JSON).
+            when().
+            get("https://jira.hillel.it/rest/api/2/issue/" + ticketId).
+            then().
+            statusCode(404).
+            extract().response();
+  }
+
+  @Test
+  public void addDeleteJiraComment(){
+    addJiraCommentResponse =
+        given().
+            auth().preemptive().basic("webinar5", "webinar5").
+            contentType(ContentType.JSON).
+            body("{\"body\" : \"New_Comment\"}").
+            when().
+            post("https://jira.hillel.it/rest/api/2/issue/WEBINAR-13562/comment").
+            then().
+            contentType(ContentType.JSON).
+            statusCode(201).
+            time(lessThan(2L), TimeUnit.SECONDS).
+            extract().response();
+    commentURI = addJiraCommentResponse.path("self");
+
+    deleteCommentResponse =
+        given().
+            auth().preemptive().basic("webinar5", "webinar5").
+            delete(commentURI).
+            then().
+            statusCode(204).
+            extract().response();
+
+    getDeletedCommentResponse =
+        given().
+            auth().preemptive().basic("webinar5", "webinar5").
+            get(commentURI).
+            then().
+            statusCode(404).
+            extract().response();
   }
 }
